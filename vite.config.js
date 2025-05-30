bf251798dd4932b8c7c4ab9b85b6eb92b1c514d0
ejcacciatore <ejcacciatore@gmail.com>
2025-05-30 13:04:53 -0400

    updated vite file and main file

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/calcguard-dashboard/'
-})
-=======
   base: '/calcguard-dashboard/',
   build: {
     outDir: 'dist',
@@ -22,4 +18,3 @@ export default defineConfig({
     'process.env.NODE_ENV': '"production"'
   }
 })
->>>>>>> d22ab79a2e9d382da08629bf7250cd4c74364a6e
